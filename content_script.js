9610b9f4f571f55cd51d383eea6176721c2fc335
tgrthrowawayaccount <thejunkthemail@gmail.com>
2025-01-28 01:05:26 -0500

    Update content_script.js

--- content_script.js
+++ content_script.js
@@ -1,5 +1,24 @@
 const text = document.getElementsByTagName("*")
 
+for (let j=0; j<6; j++){
 for (let i=0; i<text.length; i++) {
   if (text[i].innerHTML.includes('[emoji637]')) {
   text[i].innerHTML=text[i].innerHTML.replace('[emoji637]', '1' )}}
+  if (text[i].innerHTML.includes('[emoji638]')) {
+  text[i].innerHTML=text[i].innerHTML.replace('[emoji638]', '2' )}}
+  if (text[i].innerHTML.includes('[emoji639]')) {
+  text[i].innerHTML=text[i].innerHTML.replace('[emoji639]', '3' )}}
+  if (text[i].innerHTML.includes('[emoji640]')) {
+  text[i].innerHTML=text[i].innerHTML.replace('[emoji640]', '4' )}}
+  if (text[i].innerHTML.includes('[emoji641]')) {
+  text[i].innerHTML=text[i].innerHTML.replace('[emoji641]', '5' )}}
+  if (text[i].innerHTML.includes('[emoji642]')) {
+  text[i].innerHTML=text[i].innerHTML.replace('[emoji642]', '6' )}}
+  if (text[i].innerHTML.includes('[emoji643]')) {
+  text[i].innerHTML=text[i].innerHTML.replace('[emoji643]', '7' )}}
+  if (text[i].innerHTML.includes('[emoji644]')) {
+  text[i].innerHTML=text[i].innerHTML.replace('[emoji644]', '8' )}}
+  if (text[i].innerHTML.includes('[emoji645]')) {
+  text[i].innerHTML=text[i].innerHTML.replace('[emoji6345]', '9' )}
+  if (text[i].innerHTML.includes('[emoji646]')) {
+  text[i].innerHTML=text[i].innerHTML.replace('[emoji646]', '0' )}}}
