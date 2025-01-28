81bb7b6c3abef7ce1fd9d2e2acaf3f32e7931425
tgrthrowawayaccount <thejunkthemail@gmail.com>
2025-01-28 01:02:10 -0500

    Update content_script.js

--- content_script.js
+++ content_script.js
@@ -1,5 +1,5 @@
 const text = document.getElementsByTagName("*")
 
 for (let i=0; i<text.length; i++) {
-  if text[i].innerHTML.includes('[emoji637]') {
+  if (text[i].innerHTML.includes('[emoji637]')) {
   text[i].innerHTML=text[i].innerHTML.replace('[emoji637]', '1' )}}
